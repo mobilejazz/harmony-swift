810ce727c03aa6751e8d5fbb7661d9412515d1b7
Joan Martin <joan@mobilejazz.com>
2017-12-04 13:17:15 +0100

    commenting debug code

--- Example/MJSwiftCore/AppDelegate.swift
+++ Example/MJSwiftCore/AppDelegate.swift
@@ -31,19 +31,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         
-        let linkRecognizer = LinkRecognizer(scheme: "mjz", options: [.CaseInsensitive, .AnchoredStart, .AnchoredEnd])
-        
-        linkRecognizer.registerPattern("//screen/tomorrow", forKey: "tomorrow")
-        linkRecognizer.registerPattern("//screen/today/\(LinkRecognizer.Pattern.numeric)", forKey: "today")
-        linkRecognizer.registerPattern("//screen/yesterday/\(LinkRecognizer.Pattern.alphanumeric)/index", forKey: "yesterday")
-        
-        let result1 = linkRecognizer.recognize(URL(string:"mjz://screen/today/1234")!)
-        let result2 = linkRecognizer.recognize(URL(string:"mjz://screen/tomorrow")!)
-        let result3 = linkRecognizer.recognize(URL(string:"mjz://screen/yesterday/o239hf2oisdh9/index")!)
-        
-        NSLog("Result 1: \(result1)")
-        NSLog("Result 2: \(result2)")
-        NSLog("Result 3: \(result3)")
+//        let linkRecognizer = LinkRecognizer(scheme: "mjz", options: [.CaseInsensitive, .AnchoredStart, .AnchoredEnd])
+//        
+//        linkRecognizer.registerPattern("//screen/tomorrow", forKey: "tomorrow")
+//        linkRecognizer.registerPattern("//screen/today/\(LinkRecognizer.Pattern.numeric)", forKey: "today")
+//        linkRecognizer.registerPattern("//screen/yesterday/\(LinkRecognizer.Pattern.alphanumeric)/index", forKey: "yesterday")
+//        
+//        let result1 = linkRecognizer.recognize(URL(string:"mjz://screen/today/1234")!)
+//        let result2 = linkRecognizer.recognize(URL(string:"mjz://screen/tomorrow")!)
+//        let result3 = linkRecognizer.recognize(URL(string:"mjz://screen/yesterday/o239hf2oisdh9/index")!)
+//        
+//        NSLog("Result 1: \(result1)")
+//        NSLog("Result 2: \(result2)")
+//        NSLog("Result 3: \(result3)")
         
         return true
     }
