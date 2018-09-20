6fec83567467d84193f3ea90d68cc08f5947ed7d
Joan Martin <joan@mobilejazz.com>
2018-09-20 11:19:41 +0200

    adding missing files

--- Example/Pods/PromisesSwift/Sources/Promises/Promise+Wrap.swift
+++ Example/Pods/PromisesSwift/Sources/Promises/Promise+Wrap.swift
@@ -12,54 +12,62 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import Dispatch
+
 /// Provides a convenient way to convert methods that use common callback patterns into `Promise`s.
 
 /// Creates a new promise to be resolved when completion handler gets invoked.
-/// - parameter work: A block to execute on the main queue to invoke some API that requires
+/// - parameter work: A block to execute asynchronously to invoke some API that requires
 ///                   a completion handler with no arguments.
 /// - returns: A new pending promise to be resolved with `nil` when completion handler finishes.
-public func resolve(_ work: @escaping (@escaping () -> Void) throws -> Void) -> Promise<Any?> {
-  return Promise<Any?> { fulfill, _ in
+public func wrap(
+  on queue: DispatchQueue = .promises,
+  _ work: @escaping (@escaping () -> Void) throws -> Void
+) -> Promise<Any?> {
+  return Promise<Any?>(on: queue) { fulfill, _ in
     try work { fulfill(nil) }
   }
 }
 
 /// Creates a new promise to be resolved when completion handler gets invoked.
-/// - parameter work: A block to execute on the main queue to invoke some API that requires
+/// - parameter work: A block to execute asynchronously to invoke some API that requires
 ///                   a completion handler with one argument of generic `Value` type.
 /// - returns: A new pending promise to be resolved with the value provided by completion handler
 ///            when it finishes.
-public func resolve<Value>(
+public func wrap<Value>(
+  on queue: DispatchQueue = .promises,
   _ work: @escaping (@escaping (Value) -> Void) throws -> Void
 ) -> Promise<Value> {
-  return Promise<Value> { fulfill, _ in
+  return Promise<Value>(on: queue) { fulfill, _ in
     try work { fulfill($0) }
   }
 }
 
 /// Creates a new promise to be resolved when completion handler gets invoked.
-/// - parameter work: A block to execute on the main queue to invoke some API that requires
+/// - parameter work: A block to execute asynchronously to invoke some API that requires
 ///                   a completion handler with one argument of optional generic `Value` type.
 /// - returns: A new pending promise to be resolved with the value or error provided by completion
 ///            handler when it finishes.
-public func resolve<Value>(
+public func wrap<Value>(
+  on queue: DispatchQueue = .promises,
   _ work: @escaping (@escaping (Value?) -> Void) throws -> Void
 ) -> Promise<Value?> {
-  return Promise<Value?> { fulfill, _ in
+  return Promise<Value?>(on: queue) { fulfill, _ in
     try work { fulfill($0) }
   }
 }
 
 /// Creates a new promise to be resolved when completion handler gets invoked.
-/// - parameter work: A block to execute on the main queue to invoke some API that requires
+/// - parameter work: A block to execute asynchronously to invoke some API that requires
 ///                   a completion handler with two arguments: a generic of `Value` type and
 ///                   an optional `Error`.
 /// - returns: A new pending promise to be resolved with the value or error provided by completion
 ///            handler when it finishes.
-public func resolve<Value>(
+public func wrap<Value>(
+  on queue: DispatchQueue = .promises,
   _ work: @escaping (@escaping (Value, Error?) -> Void) throws -> Void
 ) -> Promise<Value> {
-  return Promise<Value> { fulfill, reject in
+  return Promise<Value>(on: queue) { fulfill, reject in
     try work { value, error in
       if let error = error {
         reject(error)
@@ -71,15 +79,16 @@ public func resolve<Value>(
 }
 
 /// Creates a new promise to be resolved when completion handler gets invoked.
-/// - parameter work: A block to execute on the main queue to invoke some API that requires
+/// - parameter work: A block to execute asynchronously to invoke some API that requires
 ///                   a completion handler with two arguments: an optional `Error` and a generic of
 ///                   `Value` type.
 /// - returns: A new pending promise to be resolved with the error or value provided by completion
 ///            handler when it finishes.
-public func resolve<Value>(
+public func wrap<Value>(
+  on queue: DispatchQueue = .promises,
   _ work: @escaping (@escaping (Error?, Value) -> Void) throws -> Void
 ) -> Promise<Value> {
-  return Promise<Value> { fulfill, reject in
+  return Promise<Value>(on: queue) { fulfill, reject in
     try work { error, value in
       if let error = error {
         reject(error)
@@ -91,15 +100,16 @@ public func resolve<Value>(
 }
 
 /// Creates a new promise to be resolved when completion handler gets invoked.
-/// - parameter work: A block to execute on the main queue to invoke some API that requires
+/// - parameter work: A block to execute asynchronously to invoke some API that requires
 ///                   a completion handler with two arguments: an optional generic of `Value` type
 ///                   and an optional `Error`.
 /// - returns: A new pending promise to be resolved with the value or error provided by completion
 ///            handler when it finishes.
-public func resolve<Value>(
+public func wrap<Value>(
+  on queue: DispatchQueue = .promises,
   _ work: @escaping (@escaping (Value?, Error?) -> Void) throws -> Void
 ) -> Promise<Value?> {
-  return Promise<Value?> { fulfill, reject in
+  return Promise<Value?>(on: queue) { fulfill, reject in
     try work { value, error in
       if let error = error {
         reject(error)
@@ -111,15 +121,16 @@ public func resolve<Value>(
 }
 
 /// Creates a new promise to be resolved when completion handler gets invoked.
-/// - parameter work: A block to execute on the main queue to invoke some API that requires
+/// - parameter work: A block to execute asynchronously to invoke some API that requires
 ///                   a completion handler with two arguments: an optional `Error` and an optional
 ///                   generic of `Value` type.
 /// - returns: A new pending promise to be resolved with the error or value provided by completion
 ///            handler when it finishes.
-public func resolve<Value>(
+public func wrap<Value>(
+  on queue: DispatchQueue = .promises,
   _ work: @escaping (@escaping (Error?, Value?) -> Void) throws -> Void
 ) -> Promise<Value?> {
-  return Promise<Value?> { fulfill, reject in
+  return Promise<Value?>(on: queue) { fulfill, reject in
     try work { error, value in
       if let error = error {
         reject(error)
@@ -131,15 +142,16 @@ public func resolve<Value>(
 }
 
 /// Creates a new promise to be resolved when completion handler gets invoked.
-/// - parameter work: A block to execute on the main queue to invoke some API that requires
+/// - parameter work: A block to execute asynchronously to invoke some API that requires
 ///                   a completion handler with three arguments: two optionals of `Any` type
 ///                   and an optional `Error`.
 /// - returns: A new pending promise to be resolved with a tuple of optional values or an error
 ///            provided by completion handler when it finishes.
-public func resolve<Value1, Value2>(
+public func wrap<Value1, Value2>(
+  on queue: DispatchQueue = .promises,
   _ work: @escaping (@escaping (Value1?, Value2?, Error?) -> Void) throws -> Void
 ) -> Promise<(Value1?, Value2?)> {
-  return Promise<(Value1?, Value2?)> { fulfill, reject in
+  return Promise<(Value1?, Value2?)>(on: queue) { fulfill, reject in
     try work { value1, value2, error in
       if let error = error {
         reject(error)
