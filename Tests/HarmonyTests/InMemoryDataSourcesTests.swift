8de53868df8062e7c9a872b826ca10c06b558242
Javi García <javier@mobilejazz.com>
2022-06-06 14:23:47 +0200

    IOS-0737 :: Feature Internal :: Set up unit tests schema (#34)
    * feat: add tests to Harmony
    
    * chore: ignoring idea folder in gitignore
    
    * chore: add development notes README
    
    Co-authored-by: Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>

---
+++ Tests/HarmonyTests/InMemoryDataSourcesTests.swift
@@ -0,0 +1,16 @@
+//
+//  InMemoryDataSourcesTests.swift
+//  HarmonyTests
+//
+//  Created by Javi on 6/6/22.
+//
+
+import Foundation
+import Nimble
+import XCTest
+
+class InMemoryDataSourcesTests: XCTestCase {
+    func test() {
+        expect(1) == 1
+    }
+}
